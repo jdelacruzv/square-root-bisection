b12806f1ca26e8ed6d314bc1ee1db1f3ce6bbeb6
Jose De La Cruz <jldlcv@gmail.com>
2024-05-31 22:45:24 +0200

    Define square_root_bisection function

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Programa que encuentra la raíz cuadrada aproximada de un número determinado utilizando 
+el método de bisección. El método de bisección es una técnica para encontrar las raíces
+de una función de valor real. Funciona reduciendo un intervalo donde se encuentra la raíz
+cuadrada hasta que converge a un valor dentro de una tolerancia especificada.
+'''
+
+def square_root_bisection():
+	pass
